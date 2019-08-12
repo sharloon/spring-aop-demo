c49a28692cda19780632761faaa3ec6fec9452b5
huangxiaolong <sharloon@163.com>
2019-08-12 20:43:59 +0800

    使用AOP+自定义注解的方式实现了将带有@SysLog注解的接口的请求添加日志记录功能 关键类： ProceedingJoinPoint：获取接口的类名和方法名等 RequestContextHolder：获取http相关的信息、客户端相关的信息等

---
+++ src/main/java/stu/hxl/aop/annotation/SysLog.java
@@ -0,0 +1,24 @@
+package stu.hxl.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 日志注解，作用于方法上，通过LogAspect记录方法的执行日志
+ *
+ * @author sharloon
+ * @date 2019/8/12
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SysLog {
+
+    /**
+     * 描述
+     *
+     * @return
+     */
+    String value() default "";
+}
